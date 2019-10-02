34d29f2b6617c809eb53c3c3a5d25e101c52a91c
Cristian <cris178@outlook.com>
2019-10-01 21:18:12 -0700

    Upload Project

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import firebase from "firebase"
+
+// PASTE FIREBASE CODE HERE (before "export default firebase;")
+// It should look similar to the following:
+// // Initialize Firebase
+// var config = {
+//   apiKey: "XXXXXXX",
+//   authDomain: "XXXXX",
+//   databaseURL: "XXXXXX",
+//   projectId: "XXXXXXX",
+//   storageBucket: "XXXXXXX",
+//   messagingSenderId: "XXXXXXX"
+// };
+// firebase.initializeApp(config);
+
+
+export default firebase;
